6e94b81c445d2779766ebf43ee9585695d3efda3
Inomata <tonyhawkingsexample@example.com>
2024-12-10 19:17:52 +0900

    backend側のAPI連携を鑑賞

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
 )
 
 func main() {
+
+	url := "https://api.themoviedb.org/3/movie/movie_id?language=en-US"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Go backend!")
+		fmt.Fprintf(w, "%s",body)
 	})
 	fmt.Println("Server running on port 8000")
 	http.ListenAndServe(":8000", nil)
